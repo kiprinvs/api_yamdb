d393c98b9b4c5cd574cf06022afddf959fe8985c
Вячеслав Киприн <kiprinvs@gmail.com>
2024-08-24 13:26:33 +0700

    fix pep

--- api_yamdb/users/validators.py
+++ api_yamdb/users/validators.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.core.exceptions import ValidationError
 
+
 def username_validator(value):
     if value in settings.BAD_USERNAMES:
         raise ValidationError(f'Недопустимое имя - {value}')
